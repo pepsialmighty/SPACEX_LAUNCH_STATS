5824d34f0a2514e3ba7cdd3a63e55a2de4164e7b
pepsialmighty <hainguyenn257@gmail.com>
2020-07-01 00:34:30 +0200

    Add Launches Functional Component

---
+++ public/build/precache-manifest.599485a02927d84953c84f8e4c2eafa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52349bcc98bb7da5bdc32ae51f9b755",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7662ce243d94dcfac399",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "00339c7602b040d9186c",
+    "url": "/static/js/2.b732429e.chunk.js"
+  },
+  {
+    "revision": "d238e78a2900068cc959425de1c25d1e",
+    "url": "/static/js/2.b732429e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7662ce243d94dcfac399",
+    "url": "/static/js/main.c3c02562.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
