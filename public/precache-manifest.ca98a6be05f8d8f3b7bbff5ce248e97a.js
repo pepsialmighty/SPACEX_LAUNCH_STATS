71f02d4cd0cb6fb5f6f06ef097a095bd5331eb7b
pepsialmighty <hainguyenn257@gmail.com>
2020-06-30 23:05:18 +0200

    Finished React

---
+++ public/precache-manifest.ca98a6be05f8d8f3b7bbff5ce248e97a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e1800ceaab87389ed0c50d9eee339ed",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aa3cb7e2e6427a80f246",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "04fbf492917ac4ef50c3",
+    "url": "/static/js/2.85921115.chunk.js"
+  },
+  {
+    "revision": "d238e78a2900068cc959425de1c25d1e",
+    "url": "/static/js/2.85921115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa3cb7e2e6427a80f246",
+    "url": "/static/js/main.4552a6c8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 const schema = require("./schema");
+const path = require("path");
 
-/* import express from "express";
-import graphqlHTTP from "express-graphql";
-import schema from "./schema"; */
+/* 
+ import express from "express";
+ import graphqlHTTP from "express-graphql";
+ import schema from "./schema"; 
+*/
 
 //initialize express
 const app = express();
 
-/* declare an endpoint /graphql that run graphqlHTTP that 
-point to our schema */
-/* graphiql is a tool that act like a client to make request
-to our server */
+//Allow cross-origin
+app.use(cors());
+
+/**
+ * ! declare an endpoint /graphql that run graphqlHTTP that
+ * ! point to our schema
+ * !graphiql is a tool that act like a client to make request
+ * ! to our server */
+
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -20,6 +29,20 @@ app.use(
     graphiql: true,
   })
 );
+
+/**
+ * ! set a static folder 'public'
+ */
+app.use(express.static("public"));
+
+/**
+ * ! any route except /graphql hit, it will be redirected back
+ * ! to index.html in public folder
+ * ? __dirname : from the current directory, then go to public then index.html
+ */
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 
